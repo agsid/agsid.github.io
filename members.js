9574a1bcb409a7fdede0bfbb7f8e0d31efdabede
SID AG <121702864+agsid@users.noreply.github.com>
2025-08-11 20:12:35 -0400

    Create members.js

---
+++ members.js
@@ -0,0 +1,38 @@
+            const members = [
+                {
+                    id: 1,
+                    name: "Sid Agrawal",
+                    role: "Build and Code",
+                    bio: " I am an 8th grader with 6 years of experience in FLL. I love to play with things that move. Outside of robotics I also play the trumpet and practice Taekwondo.",
+                    image: "images/aboutus/sid.jpg"
+                },
+                {
+                    id: 2,
+                    name: "Ariv Mulki",
+                    role: "Design Lead",
+                    bio: "  ",
+                    image: "images/aboutus/aariv.png    "
+                },
+                {
+                    id: 3,
+                    name: "Tanush Nardeddy",
+                    role: "Code Lead",
+                    bio: "",
+                    image: "images/aboutus/tanush.jpg"
+                },
+                {
+                    id: 4,
+                    name: "Ganesh Mulki",
+                    role: "Coach ",
+                    bio: "Drives brand growth", 
+                    image: "https://cdn.dribbble.com/userupload/24896180/file/original-c250b2ac5ee879252e2b7703f70ed95a.gif"
+                },
+                {
+                    id: 5,
+                    name: "Amit Agrawal",
+                    role: "Coach",
+                    bio: "Bridges the gap between technical teams",
+                    image: "https://i.pinimg.com/originals/d9/92/64/d99264eefc942c2d10ef0521ab0656ff.gif"
+                }
+            ];
+
