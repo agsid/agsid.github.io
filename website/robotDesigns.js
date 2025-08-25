b1fd630712701cc91a9fe641b8fc8f66537b8d1b
SID AG <121702864+agsid@users.noreply.github.com>
2025-08-25 17:13:32 -0400

    Create robotDesigns.js

---
+++ website/robotDesigns.js
@@ -0,0 +1,29 @@
+const robotDesigns = [
+    {
+        heading: 'Robot Design #1',
+        imageSrc: 'images/robot/BaseBot.jpg',
+        imageAlt: 'Robot Design #1',
+        teamNumberInputId: 'teamNumberInput1',
+        statusMessageId: 'statusMessage1',
+        downloadFilePath: './robot/instructions/basebot.pdf',
+        cadLink: 'https://www.bricklink.com/v3/studio/design.page?idModel=694818'
+    },
+    {
+        heading: 'Robot Design #2',
+        imageSrc: 'images/robot/BaseBot2.jpg',
+        imageAlt: 'Robot Design #2',
+        teamNumberInputId: 'teamNumberInput2',
+        statusMessageId: 'statusMessage2',
+        downloadFilePath: './robot/instructions/basebot2.pdf',
+        cadLink: 'https://www.bricklink.com/v3/studio/design.page?idModel=694816'
+    },
+    {
+        heading: 'Robot Design #3',
+        imageSrc: 'images/robot/BaseBot3.jpg',
+        imageAlt: 'Robot Design #3',
+        teamNumberInputId: 'teamNumberInput3',
+        statusMessageId: 'statusMessage3',
+        downloadFilePath: './robot/instructions/Basebot3.pdf',
+        cadLink: 'https://www.bricklink.com/v3/studio/design.page?idModel=694817'
+    }
+];
