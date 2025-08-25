9a5b661e4f796d90b6cfc5d5bc58848ab7deef9a
SID AG <121702864+agsid@users.noreply.github.com>
2025-08-25 17:22:03 -0400

    Robot

--- website/robotDesigns.js
+++ website/robotDesigns.js
@@ -1,8 +1,10 @@
 const robotDesigns = [
     {
         heading: 'Robot Design #1',
-        imageSrc: 'images/robot/BaseBot.jpg',
-        imageAlt: 'Robot Design #1',
+        images: [
+            { src: 'images/robot/BsaeBot.jpg', alt: 'Robot Design #1 - View 1' },
+            { src: 'images/robot/1basebot.jpg.jpg', alt: 'Robot Design #1 - View 2' }
+        ],
         teamNumberInputId: 'teamNumberInput1',
         statusMessageId: 'statusMessage1',
         downloadFilePath: './robot/instructions/basebot.pdf',
@@ -10,8 +12,10 @@ const robotDesigns = [
     },
     {
         heading: 'Robot Design #2',
-        imageSrc: 'images/robot/BaseBot2.jpg',
-        imageAlt: 'Robot Design #2',
+        images: [
+            { src: 'images/robot/BaseBot2.jpg', alt: 'Robot Design #2 - View 1' },
+            { src: 'images/robot/1basebot2.jpg.jpg', alt: 'Robot Design #2 - View 2' }
+        ],
         teamNumberInputId: 'teamNumberInput2',
         statusMessageId: 'statusMessage2',
         downloadFilePath: './robot/instructions/basebot2.pdf',
@@ -19,8 +23,11 @@ const robotDesigns = [
     },
     {
         heading: 'Robot Design #3',
-        imageSrc: 'images/robot/BaseBot3.jpg',
-        imageAlt: 'Robot Design #3',
+        images: [
+            { src: 'images/robot/UntitledModel.jpg', alt: 'Robot Design #3 - View 1' },
+            { src: 'images/robot/UntitledModel_2.jpg', alt: 'Robot Design #3 - View 2' },
+            { src: 'images/robot/UntitledModel_3.jpg', alt: 'Robot Design #3 - View 3' }
+        ],
         teamNumberInputId: 'teamNumberInput3',
         statusMessageId: 'statusMessage3',
         downloadFilePath: './robot/instructions/Basebot3.pdf',
