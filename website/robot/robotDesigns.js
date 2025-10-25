04fd3723192d315ea1c8257c83749f0800747525
SID AG <121702864+agsid@users.noreply.github.com>
2025-10-25 02:21:41 -0400

    hi

--- website/robot/robotDesigns.js
+++ website/robot/robotDesigns.js
@@ -2,31 +2,31 @@ const robotDesigns = [
     {
         heading: 'Robot Design #1',
         images: [
-            { src: './robot/bsaebot.jpg.png', alt: 'Robot Design #1 - View 1' },
-            { src: './robot/1basebot.jpg.png', alt: 'Robot Design #1 - View 2' }
+            { src: './pictures/bsaebot.jpg.png', alt: 'Robot Design #1 - View 1' },
+            { src: './pictures/1basebot.jpg.png', alt: 'Robot Design #1 - View 2' }
         ],
         teamNumberInputId: 'teamNumberInput1',
         statusMessageId: 'statusMessage1',
-        downloadFilePath: './robot/instructions/basebot.pdf',
+        downloadFilePath: './instructions/basebot.pdf',
         cadLink: 'https://www.bricklink.com/v3/studio/design.page?idModel=694818'
     },
     {
         heading: 'Robot Design #2',
         images: [
-            { src: './robot/1basebot2.jpg.png', alt: 'Robot Design #2 - View 1' },
-            { src: './robot/basebot2.jpg.png', alt: 'Robot Design #2 - View 2' }
+            { src: './pictures/1basebot2.jpg.png', alt: 'Robot Design #2 - View 1' },
+            { src: './pictures/basebot2.jpg.png', alt: 'Robot Design #2 - View 2' }
         ],
         teamNumberInputId: 'teamNumberInput2',
         statusMessageId: 'statusMessage2',
-        downloadFilePath: './robot/instructions/basebot2.pdf',
+        downloadFilePath: './instructions/basebot2.pdf',
         cadLink: 'https://www.bricklink.com/v3/studio/design.page?idModel=694816'
     },
     {
         heading: 'Robot Design #3',
         images: [
-            { src: './robot/UntitledModel.png', alt: 'Robot Design #3 - View 1' },
-            { src: './robot/UntitledModel_2.png', alt: 'Robot Design #3 - View 2' },
-            { src: './robot/UntitledModel_3.png', alt: 'Robot Design #3 - View 3' }
+            { src: './pictures/UntitledModel.png', alt: 'Robot Design #3 - View 1' },
+            { src: './pictures/UntitledModel_2.png', alt: 'Robot Design #3 - View 2' },
+            { src: './pictures/UntitledModel_3.png', alt: 'Robot Design #3 - View 3' }
         ],
         teamNumberInputId: 'teamNumberInput3',
         statusMessageId: 'statusMessage3',
